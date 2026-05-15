fbc44b97c42e68b377e2b2ee2220cbb75e668612
Jhonattas Smitti <smitti.j@gmail.com>
2026-05-15 10:22:26 -0300

    fix: correct relative path for bubbles types in BubbleEngine

--- apps/web/modules/blackboard/engine/bubble-engine/BubbleEngine.ts
+++ apps/web/modules/blackboard/engine/bubble-engine/BubbleEngine.ts
@@ -1,5 +1,5 @@
 import { BLACKBOARD_EVENTS, blackboardEventBus } from '../../events/eventBus';
-import { BlackboardBubble, BubbleType } from '../types/bubbles';
+import { BlackboardBubble, BubbleType } from '../../types/bubbles';
 import { attributeEngine } from '../attribute-engine/AttributeEngine';
 
 class BubbleEngine {
